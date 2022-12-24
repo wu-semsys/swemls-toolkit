1786abbc18353f0f85daae692cd86d375cc727f2
Fajar J. Ekaputra <fajar.juang@gmail.com>
2022-12-24 16:34:17 +0100

    code and data refactoring

--- src/main/java/id/semantics/shacl/SwemlsTookit.java
+++ src/main/java/id/semantics/shacl/SwemlsTookit.java
@@ -3,7 +3,9 @@ package id.semantics.shacl;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
+import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
@@ -15,47 +17,54 @@ public class SwemlsTookit {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        String validationMessage = "validation failed";
-
-        String patternsFolder = "input/patterns/";
-        String shapesFolder = "input/shapes/";
-
-        String swemlsPrefix = "swemls";
-        String swemlsNS = "https://w3id.org/semsys/ns/swemls#";
-        String date = "15122022";
-
-        String rawSWeMLSdata = "input/data/pattern-instances-" + date + ".ttl";
-        String swCompoundFile = "input/data/sw-compound-mapping-2911-0023.ttl";
-        String mlCompoundFile = "input/data/ml-compound-mapping-2911-0011.ttl";
-        String krCompoundFile = "input/data/kr-compound-mapping-2911-0011.ttl";
-
-        String ontologyFile = "input/swemls-ontology-" + date + ".ttl";
-
+        String ontologyFile = "https://w3id.org/semsys/ns/swemls/ontology.ttl";
         String outputFolder = "output/";
-        String ruleExecutionResults = outputFolder + "rule_results-" + date + ".ttl";
-        String constraintExecutionResults = outputFolder + "constraint_results-" + date + ".ttl";
-        String kgOutput = outputFolder + "swemls-kg-" + date + ".ttl";
+        String inputFolder = "input/";
+
+        String patternsFolder = inputFolder + "patterns/";
+        String shapesFolder = inputFolder + "shapes/";
+        String dataFolder = inputFolder + "data/";
+
+        String inputInstances = dataFolder + "pattern-instances-15122022.ttl";
+        String inputSWCompounds = dataFolder + "sw-compound-mapping-2911-0023.ttl";
+        String inputMLCompounds = dataFolder + "ml-compound-mapping-2911-0011.ttl";
+        String inputKRCompounds = dataFolder + "kr-compound-mapping-2911-0011.ttl";
+
+        String combinedInstancesOutput = outputFolder + "swemls-sms-metadata.ttl";
+        String inferredInstancesOutput = outputFolder + "generated-statements.ttl";
+        String shapesOutput = outputFolder + "swemls-shapes.ttl";
+        String enhancementOutput = outputFolder + "enrichments-results.ttl";
+        String swemlsKGOutput = outputFolder + "SWeMLS-KG.ttl";
+
+        String validationReport = outputFolder + "validation-reports.ttl";
 
         // load SHACL constraints for patterns
         Model shapesGraph = SwissKnife.initAndLoadModelFromFilesInFolder(shapesFolder, Lang.TURTLE);
+        RDFDataMgr.write(new FileOutputStream(shapesOutput), shapesGraph, Lang.TURTLE);
 
-        // load data
-        Model dataGraph = SwissKnife.initAndLoadModelFromResource(rawSWeMLSdata, Lang.TURTLE);
+        // load data & ontology
+        Model ontoGraph = SwissKnife.initAndLoadModelFromURL(ontologyFile, Lang.TURTLE);
+        Model dataGraph = SwissKnife.initAndLoadModelFromResource(inputInstances, Lang.TURTLE);
 
-        // add ontology to dataGraph and set swemls prefix
-        dataGraph.add(SwissKnife.initAndLoadModelFromResource(ontologyFile, Lang.TURTLE));
-        dataGraph.setNsPrefix(swemlsPrefix, swemlsNS);
+        // add all compound files to datagraph
+        dataGraph.add(SwissKnife.initAndLoadModelFromResource(inputSWCompounds, Lang.TURTLE));
+        dataGraph.add(SwissKnife.initAndLoadModelFromResource(inputKRCompounds, Lang.TURTLE));
+        dataGraph.add(SwissKnife.initAndLoadModelFromResource(inputMLCompounds, Lang.TURTLE));
+        RDFDataMgr.write(new FileOutputStream(combinedInstancesOutput), dataGraph, Lang.TURTLE);
 
-        // executing SHACL-AF rules for generating workflow and write the results to an
-        // output file
-        Model ruleResults = RuleUtil.executeRules(dataGraph, shapesGraph, dataGraph, null);
-        RDFDataMgr.write(new FileOutputStream(ruleExecutionResults), ruleResults, Lang.TURTLE);
+        // initialise inference graphs for validation
+        Model inferenceGraph = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RDFS_INF);
+        inferenceGraph.add(ontoGraph);
+        inferenceGraph.add(dataGraph);
+        inferenceGraph.setNsPrefixes(ontoGraph.getNsPrefixMap());
+        RDFDataMgr.write(new FileOutputStream(inferredInstancesOutput), inferenceGraph, Lang.TURTLE);
 
-        // add rule execution results to datagraphs
-        dataGraph.add(ruleResults);
+        // executing SHACL-AF rules for generating workflow and write the results to an output file
+        Model ruleResults = RuleUtil.executeRules(inferenceGraph, shapesGraph, inferenceGraph, null);
+        RDFDataMgr.write(new FileOutputStream(enhancementOutput), ruleResults, Lang.TURTLE);
 
         // execute SHACL constraints on dataGraphs
-        Resource validationResult = ValidationUtil.validateModel(dataGraph, shapesGraph, false);
+        Resource validationResult = ValidationUtil.validateModel(inferenceGraph, shapesGraph, false);
 
         // write validation report and set SHACL namespace
         Model shaclValidationReport = validationResult.getModel();
@@ -63,24 +72,30 @@ public class SwemlsTookit {
 
         // check if the validation successful
         if (shaclValidationReport.containsLiteral(null, SH.conforms, false)) {
-            System.out.println(validationMessage);
-            RDFDataMgr.write(new FileOutputStream(constraintExecutionResults), shaclValidationReport, Lang.TURTLE);
+            System.out.println("Validation Failed! See validation results at " + validationReport);
 
-            // generate final knowledge graphs
+        // generate final knowledge graphs
         } else {
-
-            // add pattern graph to data
+            // load all paterns (and write it to a separate file)
             Model patternGraph = SwissKnife.initAndLoadModelFromFilesInFolder(patternsFolder, Lang.TURTLE);
-            dataGraph.add(patternGraph);
 
-            // add SW, KR & ML compounds to KG
-            dataGraph.add(SwissKnife.initAndLoadModelFromResource(swCompoundFile, Lang.TURTLE));
-            dataGraph.add(SwissKnife.initAndLoadModelFromResource(krCompoundFile, Lang.TURTLE));
-            dataGraph.add(SwissKnife.initAndLoadModelFromResource(mlCompoundFile, Lang.TURTLE));
+            // create an integrated KG
+            Model finalKG = ModelFactory.createDefaultModel();
+            finalKG.add(dataGraph);
+            finalKG.add(ruleResults);
+            finalKG.add(patternGraph);
+
+            // add all compound files
+            finalKG.add(SwissKnife.initAndLoadModelFromResource(inputSWCompounds, Lang.TURTLE));
+            finalKG.add(SwissKnife.initAndLoadModelFromResource(inputKRCompounds, Lang.TURTLE));
+            finalKG.add(SwissKnife.initAndLoadModelFromResource(inputMLCompounds, Lang.TURTLE));
 
             // save to file
-            RDFDataMgr.write(new FileOutputStream(kgOutput), dataGraph, Lang.TURTLE);
+            RDFDataMgr.write(new FileOutputStream(swemlsKGOutput), finalKG, Lang.TURTLE);
+
+            System.out.println("Validation Successful! See completed KG at " + swemlsKGOutput);
         }
 
+        RDFDataMgr.write(new FileOutputStream(validationReport), shaclValidationReport, Lang.TURTLE);
     }
 }
--- src/main/java/id/semantics/shacl/SwissKnife.java
+++ src/main/java/id/semantics/shacl/SwissKnife.java
@@ -11,8 +11,6 @@ import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 
 import java.io.*;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 public class SwissKnife {
 
@@ -20,14 +18,20 @@ public class SwissKnife {
         return new File(SwissKnife.class.getClassLoader().getResource(fileName).getFile());
     }
 
-    public static Model initAndLoadModelFromInput(String dataModelFile, Lang lang) throws FileNotFoundException {
-        InputStream dataModelIS = new FileInputStream(dataModelFile);
-        Model dataModel = ModelFactory.createDefaultModel();
-        RDFDataMgr.read(dataModel, dataModelIS, lang);
+    public static Model initAndLoadModelFromURL(String dataModelURL, Lang lang) throws FileNotFoundException {
+        Model dataModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+        dataModel.read(dataModelURL, null);
         return dataModel;
     }
 
     public static Model initAndLoadModelFromResource(String dataModelFile, Lang lang) throws FileNotFoundException {
+        InputStream dataModelIS = new FileInputStream(dataModelFile);
+        Model dataModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+        RDFDataMgr.read(dataModel, dataModelIS, lang);
+        return dataModel;
+    }
+
+    public static Model initAndLoadModelFromResourceWithInference(String dataModelFile, Lang lang) throws FileNotFoundException {
         InputStream dataModelIS = new FileInputStream(dataModelFile);
         Model dataModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RDFS_INF);
         RDFDataMgr.read(dataModel, dataModelIS, lang);
@@ -35,9 +39,6 @@ public class SwissKnife {
     }
 
     public static Model initAndLoadModelFromFilesInFolder(String folder, Lang lang) {
-        // ClassLoader loader = SwissKnife.class.getClassLoader();
-        // URL url = loader.getResource(folder);
-        // String path = url.getPath();
         File[] files = new File(folder).listFiles();
         Model dataModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
 
@@ -45,22 +46,6 @@ public class SwissKnife {
             dataModel.add(RDFDataMgr.loadModel(f.getAbsolutePath(), lang));
         }
 
-        // InputStream dataModelIS = SwissKnife.class.getClassLoader().getResourceAsStream(dataModelFile);
-        // RDFDataMgr.read(dataModel, dataModelIS, lang);
         return dataModel;
-    }
-
-    public static Dataset initAndLoadDatasetFromResource(String dataModelFile, Lang lang) {
-        InputStream dataModelIS = SwissKnife.class.getClassLoader().getResourceAsStream(dataModelFile);
-        Dataset dataset = DatasetFactory.create();
-        RDFDataMgr.read(dataset, dataModelIS, lang);
-        return dataset;
-    }
-
-    public static ParameterizedSparqlString initAndLoadQueryFromResource(String queryFile) throws IOException {
-        InputStream dataModelIS = SwissKnife.class.getClassLoader().getResourceAsStream(queryFile);
-        String string = IOUtils.toString(dataModelIS, Charset.forName("UTF-8"));
-        ParameterizedSparqlString query = new ParameterizedSparqlString(string);
-        return query;
     }
 }
